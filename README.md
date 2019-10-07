b3821b91b4cd6803fcb2e8a3d9e1143d8ef26f06
Mostafizur rahman <mrshimul000@gmail.com>
2019-10-07 14:05:46 +0600

    Fix some line and add readme text

---
+++ README.md
@@ -0,0 +1,11 @@
+### Problem solving with PHP
+---
+problem and solve path
+```
+Root/
+  -
+  - Folder/
+        -  ----- problem.md (Question)
+        -  ----- solve.php  (Answer)
+
+```
--- bigsum/solve.php
+++ bigsum/solve.php
@@ -1,33 +1,34 @@
 <?php
 
 
-// 
+// Input pairs are seperated by new line and pair numbers are seperated by comma(,)
 $input = "199,911
 119,991
 24,13";
 
+// empty var
 $output = '';
 
 //split pairs by new line
 $array = explode("\n", $input);
 
-// working with pairs one by one
+// working with pairs one by one through loop
 for ($i=0; $i < count($array); $i++) { 
-  // split the pair separated by comma
-  $numbers = explode(',', $array[$i]);
+    // split the pair separated by comma
+    $numbers = explode(',', $array[$i]);
 
-  // reverse these number
-   $input_one_rev = strrev($numbers[0]);
-   $input_two_rev = strrev($numbers[1]);
+    // reverse these number
+    $input_one_rev = strrev($numbers[0]);
+    $input_two_rev = strrev($numbers[1]);
 
-   // sum of these reversed numbers
-   $bigsum = (int)$input_one_rev + (int)$input_two_rev;
+    // sum of these reversed numbers
+    $bigsum = (int)$input_one_rev + (int)$input_two_rev;
 
-  // reverse this sum
-  $sum_rev = strrev($bigsum);
+    // reverse this sum
+    $sum_rev = strrev($bigsum);
 
-  // convert numbers into an array
-  $split_sum = str_split($sum_rev);
+    // convert numbers into an array
+    $split_sum = str_split($sum_rev);
 
   // check sum leading 0, if have then remove
   for ($j=0; $j < count($split_sum); $j++) { 
